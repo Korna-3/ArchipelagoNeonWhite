078480caa5bdcb30bad8420e8012859f28d13fc4
Korna-3 <obliveator@gmail.com>
2026-07-01 18:39:59 -0400

    Logic update
    Updated logic and made heavenly delight tickets filler

---
+++ worlds/neonwhite/neonwhite/locations.py
@@ -0,0 +1,314 @@
+# Locations are specific points that you would obtain an item at.
+from BaseClasses import Location
+
+
+class NWLocation(Location):
+    game: str = "Neon White"
+    starting_id: int = 500
+
+
+neon_white_levels_normal = [
+    "Movement",
+    "Pummel",
+    "Gunner",
+    "Cascade",
+    "Elevate",
+    "Bounce",
+    "Purify",
+    "Climb",
+    "Fasttrack",
+    "Glass Port",
+    "Take Flight",
+    "Godspeed",
+    "Dasher",
+    "Thrasher",
+    "Outstretched",
+    "Smackdown",
+    "Catwalk",
+    "Fastlane",
+    "Distinguish",
+    "Dancer",
+    "Guardian",
+    "Stomp",
+    "Jumper",
+    "Dash Tower",
+    "Descent",
+    "Driller",
+    "Canals",
+    "Sprint",
+    "Mountain",
+    "Superkinetic",
+    "Arrival",
+    "Forgotten City",
+    "Fireball",
+    "Ringer",
+    "Cleaner",
+    "Warehouse",
+    "Boom",
+    "Streets",
+    "Steps",
+    "Demolition",
+    "Arcs",
+    "Apartment",
+    "Hanging Gardens",
+    "Tangled",
+    "Waterworks",
+    "Killswitch",
+    "Falling",
+    "Shocker",
+    "Bouquet",
+    "Prepare",
+    "Triptrack",
+    "Race",
+    "Bubble",
+    "Shield",
+    "Overlook",
+    "Pop",
+    "Minefield",
+    "Mimic",
+    "Trigger",
+    "Greenhouse",
+    "Sweep",
+    "Fuse",
+    "Heaven's Edge",
+    "Zipline",
+    "Swing",
+    "Chute",
+    "Crash",
+    "Ascent",
+    "Straightaway",
+    "Firecracker",
+    "Streak",
+    "Mirror",
+    "Escalation",
+    "Bolt",
+    "Godstreak",
+    "Plunge",
+    "Mayhem",
+    "Barrage",
+    "Estate",
+    "Trapwire",
+    "Ricochet",
+    "Fortress",
+    "Holy Ground",
+    "Spree",
+    "Breakthrough",
+    "Glide",
+    "Closer",
+    "Hike",
+    "Switch",
+    "Access",
+    "Congregation",
+    "Sequence",
+    "Marathon",
+]
+
+neon_white_levels_giftless = [
+    "The Clocktower",
+    "The Third Temple",
+    "Sacrifice",
+    "Absolution"
+]
+
+neon_white_levels_sidequests = [  # No gifts either
+    "Elevate Traversal I",
+    "Elevate Traversal II",
+    "Purify Traversal",
+    "Godspeed Traversal",
+    "Stomp Traversal",
+    "Fireball Traversal",
+    "Dominion Traversal",
+    "Book of Life Traversal",
+    "Doghouse",
+    "Choker",
+    "Chain",
+    "Hellevator",
+    "Razor",
+    "All Seeing Eye",
+    "Resident Saw I",
+    "Resident Saw II",
+    "Sunset Flip Powerbomb",
+    "Balloon Mountain",
+    "Climbing Gym",
+    "Fisherman Suplex",
+    "STF",
+    "Arena",
+    "Attitude Adjustment",
+    "Rocket"
+]
+
+neon_white_levels_medals = [
+    "Bronze",
+    "Silver",
+    "Gold",
+    "Ace",
+    "Dev"
+]
+
+# fmt: off
+neon_white_level_name_internal = {
+    "Movement": "TUT_MOVEMENT",
+    "Pummel": "TUT_SHOOTINGRANGE",
+    "Gunner": "SLUGGER",
+    "Cascade": "TUT_FROG",
+    "Elevate": "TUT_JUMP",
+    "Bounce": "GRID_TUT_BALLOON",
+    "Purify": "TUT_BOMB2",
+    "Climb": "TUT_BOMBJUMP",
+    "Fasttrack": "TUT_FASTTRACK",
+    "Glass Port": "GRID_PORT",
+    "Take Flight": "GRID_PAGODA",
+    "Godspeed": "TUT_RIFLE",
+    "Dasher": "TUT_RIFLEJOCK",
+    "Thrasher": "TUT_DASHENEMY",
+    "Outstretched": "GRID_JUMPDASH",
+    "Smackdown": "GRID_SMACKDOWN",
+    "Catwalk": "GRID_MEATY_BALLOONS",
+    "Fastlane": "GRID_FAST_BALLOON",
+    "Distinguish": "GRID_DRAGON2",
+    "Dancer": "GRID_DASHDANCE",
+    "Guardian": "TUT_GUARDIAN",
+    "Stomp": "TUT_UZI",
+    "Jumper": "TUT_JUMPER",
+    "Dash Tower": "TUT_BOMB",
+    "Descent": "GRID_DESCEND",
+    "Driller": "GRID_STAMPEROUT",
+    "Canals": "GRID_CRUISE",
+    "Sprint": "GRID_SPRINT",
+    "Mountain": "GRID_MOUNTAIN",
+    "Superkinetic": "GRID_SUPERKINETIC",
+    "Arrival": "GRID_ARRIVAL",
+    "Forgotten City": "FLOATING",
+    "The Clocktower": "GRID_BOSS_YELLOW",
+    "Fireball": "GRID_HOPHOP",
+    "Ringer": "GRID_RINGER_TUTORIAL",
+    "Cleaner": "GRID_RINGER_EXPLORATION",
+    "Warehouse": "GRID_HOPSCOTCH",
+    "Boom": "GRID_BOOM",
+    "Streets": "GRID_SNAKE_IN_MY_BOOT",
+    "Steps": "GRID_FLOCK",
+    "Demolition": "GRID_BOMBS_AHOY",
+    "Arcs": "GRID_ARCS",
+    "Apartment": "GRID_APARTMENT",
+    "Hanging Gardens": "TUT_TRIPWIRE",
+    "Tangled": "GRID_TANGLED",
+    "Waterworks": "GRID_HUNT",
+    "Killswitch": "GRID_CANNONS",
+    "Falling": "GRID_FALLING",
+    "Shocker": "TUT_SHOCKER2",
+    "Bouquet": "TUT_SHOCKER",
+    "Prepare": "GRID_PREPARE",
+    "Triptrack": "GRID_TRIPMAZE",
+    "Race": "GRID_RACE",
+    "Bubble": "TUT_FORCEFIELD2",
+    "Shield": "GRID_SHIELD",
+    "Overlook": "SA L VAGE2",
+    "Pop": "GRID_VERTICAL",
+    "Minefield": "GRID_MINEFIELD",
+    "Mimic": "TUT_MIMIC",
+    "Trigger": "GRID_MIMICPOP",
+    "Greenhouse": "GRID_SWARM",
+    "Sweep": "GRID_SWITCH",
+    "Fuse": "GRID_TRAPS2",
+    "Heaven's Edge": "TUT_ROCKETJUMP",
+    "Zipline": "TUT_ZIPLINE",
+    "Swing": "GRID_CLIMBANG",
+    "Chute": "GRID_ROCKETUZI",
+    "Crash": "GRID_CRASHLAND",
+    "Ascent": "GRID_ESCALATE",
+    "Straightaway": "GRID_SPIDERCLAUS",
+    "Firecracker": "GRID_FIRECRACKER_2",
+    "Streak": "GRID_SPIDERMAN",
+    "Mirror": "GRID_DESTRUCTION",
+    "Escalation": "GRID_HEAT",
+    "Bolt": "GRID_BOLT",
+    "Godstreak": "GRID_PON",
+    "Plunge": "GRID_CHARGE",
+    "Mayhem": "GRID_MIMICFINALE",
+    "Barrage": "GRID_BARRAGE",
+    "Estate": "GRID_1GUN",
+    "Trapwire": "GRID_HECK",
+    "Ricochet": "GRID_ANTFARM",
+    "Fortress": "GRID_FORTRESS",
+    "Holy Ground": "GRID_GODTEMPLE_ENTRY",
+    "The Third Temple": "GRID_BOSS_GODSDEATHTEMPLE",
+    "Spree": "GRID_EXTERMINATOR",
+    "Breakthrough": "GRID_FEVER",
+    "Glide": "GRID_SKIPSLIDE",
+    "Closer": "GRID_CLOSER",
+    "Hike": "GRID_HIKE",
+    "Switch": "GRID_SKIP",
+    "Access": "GRID_CEILING",
+    "Congregation": "GRID_BOOP",
+    "Sequence": "GRID_TRIPRAP",
+    "Marathon": "GRID_ZIPRAP",
+    "Sacrifice": "TUT_ORIGIN",
+    "Absolution": "GRID_BOSS_RAPTURE",
+    "Elevate Traversal I": "SIDEQUEST_OBSTACLE_PISTOL",
+    "Elevate Traversal II": "SIDEQUEST_OBSTACLE_PISTOL_SHOOT",
+    "Purify Traversal": "SIDEQUEST_OBSTACLE_MACHINEGUN",
+    "Godspeed Traversal": "SIDEQUEST_OBSTACLE_RIFLE_2",
+    "Stomp Traversal": "SIDEQUEST_OBSTACLE_UZI2",
+    "Fireball Traversal": "SIDEQUEST_OBSTACLE_SHOTGUN",
+    "Dominion Traversal": "SIDEQUEST_OBSTACLE_ROCKETLAUNCHER",
+    "Book of Life Traversal": "SIDEQUEST_RAPTURE_QUEST",
+    "Doghouse": "SIDEQUEST_DODGER",
+    "Choker": "GRID_GLASSPATH",
+    "Chain": "GRID_GLASSPATH2",
+    "Hellevator": "GRID_HELLVATOR",
+    "Razor": "GRID_GLASSPATH3",
+    "All Seeing Eye": "SIDEQUEST_ALL_SEEING_EYE",
+    "Resident Saw I": "SIDEQUEST_RESIDENTSAWB",
+    "Resident Saw II": "SIDEQUEST_RESIDENTSAW",
+    "Sunset Flip Powerbomb": "SIDEQUEST_SUNSET_FLIP_POWERBOMB",
+    "Balloon Mountain": "GRID_BALLOONLAIR",
+    "Climbing Gym": "SIDEQUEST_BARREL_CLIMB",
+    "Fisherman Suplex": "SIDEQUEST_FISHERMAN_SUPLEX",
+    "STF": "SIDEQUEST_STF",
+    "Arena": "SIDEQUEST_ARENASIXNINE",
+    "Attitude Adjustment": "SIDEQUEST_ATTITUDE_ADJUSTMENT",
+    "Rocket": "SIDEQUEST_ROCKETGODZ",
+}
+
+# fmt: on
+
+def neon_white_get_locations() -> dict[str, int]:
+    locations_dict: dict[str, int] = {}
+    level_id = NWLocation.starting_id
+
+    for normal_level in neon_white_levels_normal:
+        for medal in neon_white_levels_medals:
+            locations_dict[normal_level + " " + medal + " Completion"] = level_id
+            level_id += 1
+        locations_dict[normal_level + " Gift"] = level_id
+        level_id += 1
+
+    for giftless_level in neon_white_levels_giftless:
+        for medal in neon_white_levels_medals:
+            locations_dict[giftless_level + " " + medal + " Completion"] = level_id
+            level_id += 1
+
+    for sidequest_level in neon_white_levels_sidequests:
+        locations_dict[sidequest_level + " Completion"] = level_id
+        level_id += 1
+
+    return locations_dict
+
+
+checks_in_sets_lvl = {
+                         lvl: {
+                             lvl + " " + medal + " Completion" for medal in neon_white_levels_medals
+                         } for lvl in neon_white_levels_normal
+                     } | {
+                         lvl: {
+                             lvl + " Gift"
+                         } for lvl in neon_white_levels_normal
+                     } | {
+                         lvl: {
+                             lvl + " " + medal + " Completion" for medal in neon_white_levels_medals
+                         } for lvl in neon_white_levels_giftless
+                     } | {
+                         lvl: {
+                             lvl + " Completion"
+                         } for lvl in neon_white_levels_sidequests
+                     }
---
+++ worlds/neonwhite/neonwhite/options.py
@@ -0,0 +1,181 @@
+from dataclasses import dataclass
+
+from Options import Choice, DeathLink, DefaultOnToggle, PerGameCommonOptions, Range, StartInventoryPool, Toggle
+
+
+class KnowledgeDifficulty(Choice):
+    """
+    How much understanding of the games mechanics is expected to figure out the solution to a given medal/gift/etc.
+    Vanilla: *Only* allow cards and strats the basegame shows you.
+    Casual: Expects the bare minimum, only tricks that are taught with in-game tutorials to beat the vanilla game.
+    Standard: Incorporates hidden but usually intended or otherwise easy to intuit tech/ideas.
+    Expert: Further includes some more advanced tech that is often only relevant to speedrunners.
+    Master: Encompasses everything else from level-specific tech to extremely niche game quirks or nuances.
+    """
+    display_name = "Knowledge Difficulty"
+    option_vanilla = 1
+    option_casual = 2
+    option_standard = 3
+    option_expert = 4
+    option_master = 5
+    default = 3
+
+class ExecutionDifficulty(Choice):
+    """
+    How hard it is to actually execute a given solution to a given medal/gift/etc, assuming all knowledge.
+    Vanilla: *Only* allow cards and strats the basegame shows you.
+    Casual: Can be done by a brand-new player with minimal hassle.
+    Standard: Consistently doable by a player who has all aces, especially players with dev medals.
+    Expert: Doable by a player who has all dev medals and is competent with advanced tech.
+    Master: Unreasonably difficult solutions that are inconsistent/overly precise to the vast majority of players.
+    """
+    display_name = "Execution Difficulty"
+    option_vanilla = 1
+    option_casual = 2
+    option_standard = 3
+    option_expert = 4
+    option_master = 5
+    default = 3
+
+class BoofShenanigans(Toggle):
+    """
+    Whether to include things like clips and far off Book of Life usages in the logic.
+    === DOES NOT DO ANYTHING ATM ===
+    """
+    display_name = "Book of Life Shenanigans"
+
+class Gifts(DefaultOnToggle):
+    """
+    Whether or not to make gifts checks.
+    """
+
+class Sidequests(DefaultOnToggle):
+    """
+    Whether or not to make sidequest completions checks.
+    """
+
+class MissionUnlockMethod(Choice):
+    """
+    How missions are unlocked to progress through the game.
+    Ranks: Fills the pool with Neon Ranks, each mission requiring a number of ranks to unlock. See Rank Requirement.
+    Missions: 1 Mission Unlock item is added to the pool per mission, each obtained unlocking the next locked mission.
+    Levels: Each level is an item that must be collected
+    """
+    display_name = "Mission Unlock Method"
+    option_ranks = 1
+    option_missions = 2
+    option_levels = 3
+    default = 1
+
+class StartingLevelCount(Range):
+    """
+    The amount of Levels for you to start with for Levels unlock method.
+    This also influences how many fist-only levels are added early into the level pool for other unlock methods.
+    """
+    display_name = "Starting Level Count"
+    range_start = 1
+    default = 5
+    range_end = 10
+
+class MedalCap(Choice):
+    """
+    The highest medal to count for checks.
+    Higher settings will result in more checks if Progressive Checks is enabled.
+    """
+    display_name = "Medal Cap"
+    option_bronze = 1
+    option_silver = 2
+    option_gold = 3
+    option_ace = 4
+    option_dev = 5
+    default = 4
+
+class RankRequirement(Range):
+    """
+    The percentage of ranks required for the final mission out of the total amount of remaining checks in the pool.
+    The rest of the mission requirements will scale accordingly.
+    Only applies when Mission Unlock Method is set to Ranks.
+    """
+    display_name = "Rank Requirement"
+    range_start = 1
+    default = 30
+    range_end = 100
+
+
+class MissionCount(Range):
+    """
+    The amount of Missions for the game to have when ranks or mission unlock method is chosen.
+    Spreads evenly across all levels, then spreads across the later half with the remainder.
+    """
+    display_name = "Mission Count"
+    range_start = 3
+    default = 11
+    range_end = 60
+
+class LevelGradient(Range):
+    """
+    The amount of "variance" in % to have in the random level selection for the ranks/mission unlock methods.
+    A lower value means, as the levels are selected, you will, on average, need more and more cards.
+    A higher value means much more randomness in the selection.
+    """
+    display_name = "Level Selection Variance"
+    range_start = 0
+    range_end = 100
+    default = 50
+
+class ProgressiveChecks(DefaultOnToggle):
+    """
+    If every medal up to the medal cap should count for checks, or if only 1 check occurs for achieving the cap medal.
+    If off, the number of checks will not change with changes to the medal cap.
+    === DOES NOT DO ANYTHING ATM ===
+    """
+    display_name = "Progressive Checks"
+
+class Traps(DefaultOnToggle):
+    """
+    Whether negative effects on the Neon White world are added to the item pool.
+    === DOES NOT DO ANYTHING ATM ===
+    """
+    display_name = "Traps"
+
+class Goal(Choice):
+    """
+    What the goal to complete should be.
+    3bosses - Beat The Clocktower, The Third Temple, and Absolution with the goal medal cap.
+    TrueEnding - Gather all memories and write Green into the Book of Life. (WILL FAIL TO GEN)
+    """
+    display_name = "Goal"
+    option_3bosses = 1
+    option_trueending = 2
+    default = 1
+
+class BossesCap(MedalCap):
+    """
+    The medal cap to use for the bosses if running the 3 bosses goal.
+    """
+    display_name = "3 Bosses Medal Cap"
+
+class NeonWhiteDeathLink(DeathLink):
+    __doc__ = (DeathLink.__doc__ + "\n\n    You can disable this or set it to give yourself a trap effect when " +  # pyright: ignore[reportOptionalOperand]
+               "another player dies in the in-game mod options. === DOES NOT DO ANYTHING ATM ===")
+
+
+@dataclass
+class NeonWhiteOptions(PerGameCommonOptions):
+    start_inventory_from_pool: StartInventoryPool
+    difficulty_knowledge: KnowledgeDifficulty
+    difficulty_execution: ExecutionDifficulty
+    medal_cap: MedalCap
+    gifts: Gifts
+    sidequests: Sidequests
+    unlock_method: MissionUnlockMethod
+    rank_requirement: RankRequirement
+    mission_count: MissionCount
+    level_gradient: LevelGradient
+    starting_level_count: StartingLevelCount
+    goal: Goal
+    bosses_goal_cap: BossesCap
+    boof_shenanigans: BoofShenanigans
+    progressive_checks: ProgressiveChecks
+    death_link: NeonWhiteDeathLink
+    bad_effects: Traps
